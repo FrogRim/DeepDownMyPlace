f72ed9609d8de0a2f3fc1bc2861ed32444eb5ad2
krlee354 <127326495+FrogRim@users.noreply.github.com>
2023-07-27 02:34:19 +0900

    07/27

--- DeepDownMyPlace/Assets/Player_Move.cs
+++ DeepDownMyPlace/Assets/Player_Move.cs
@@ -1,59 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-
 
 public class Player_Move : MonoBehaviour
 {
-    // 플레이어의 이동 속도를 조절하는 변수
-    public float speed = 5f;
-
-    
+    public float moveSpeed = 5f; // 이동 속도 조절 변수
     private Rigidbody2D rb;
-    private Vector2 target;
 
-   
-    void Start()
+    private void Start()
     {
-        // 플레이어의 Rigidbody2D 컴포넌트를 가져와서 rb 변수에 저장합니다.
         rb = GetComponent<Rigidbody2D>();
-
-        // 처음에는 플레이어의 현재 위치를 target 변수에 저장합니다.
-        target = transform.position;
     }
 
-    
-    void Update()
+    private void FixedUpdate()
     {
-        // 마우스 왼쪽 버튼을 클릭하면
-        if (Input.GetMouseButtonDown(0))
-        {
-            // 마우스의 현재 위치를 화면 좌표계에서 월드 좌표계로 변환하고 target 변수에 저장합니다.
-            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // 입력을 받아 이동
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-            
+        // normalized를 사용하여 대각선 이동 속도가 너무 빨라지는 것을 방지
+        Vector2 moveDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+        // 캐릭터를 이동 방향으로 회전
+        if (moveDirection != Vector2.zero)
+        {
+            // 좌우 이동 시에만 회전
+            if (Mathf.Abs(horizontalInput) > 0f)
+            {
+                float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+                transform.rotation = Quaternion.Euler(0f, (horizontalInput > 0f) ? 180f : 0f, 0f);
+            }
         }
+
+        // Rigidbody를 사용하여 이동
+        rb.velocity = moveDirection * moveSpeed;
     }
 
-    // FixedUpdate 함수는 고정된 시간 간격마다 실행됩니다. 물리적 연산은 여기서 처리해야 합니다.
-    void FixedUpdate()
-    {
-        // 플레이어의 현재 위치와 target 위치 사이의 거리를 계산합니다.
-        float distance = Vector2.Distance(transform.position, target);
-
-        // 거리가 0.1f보다 크면 (아직 도착하지 않았으면)
-        if (distance > 0.1f)
-        {
-            // 플레이어의 현재 위치와 target 위치 사이의 방향 벡터를 구합니다.
-            Vector2 direction = (target - (Vector2)transform.position).normalized;
-
-            // 방향 벡터에 속도를 곱하여 이동 벡터를 구합니다.
-            Vector2 movement = direction * speed * Time.fixedDeltaTime;
-
-            // Rigidbody2D 컴포넌트의 MovePosition 함수를 사용하여 플레이어를 이동시킵니다.
-            rb.MovePosition((Vector2)transform.position + movement);
-        }
-    }
 }
-
